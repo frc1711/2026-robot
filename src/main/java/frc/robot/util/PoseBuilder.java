229b1c1d61bb1c81fbba7451d4035b7693fe1ce7
Trevor Sears <trevor@trevorsears.com>
2026-03-18 19:02:42 -0400

    Removed unused import from `PoseBuilder`.

--- src/main/java/frc/robot/util/PoseBuilder.java
+++ src/main/java/frc/robot/util/PoseBuilder.java
@@ -14,7 +14,6 @@ import java.util.function.Function;
 import java.util.function.IntSupplier;
 import java.util.function.Supplier;
 
-import static edu.wpi.first.units.Units.*;
 import static frc.robot.configuration.Direction.*;
 import static frc.robot.util.PoseBuilder.CoordinateSystem.*;
 
