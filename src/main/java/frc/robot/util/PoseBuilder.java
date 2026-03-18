2661ff5f010e69a43e4a9c6e6cf21c6f70dccab3
Trevor Sears <trevor@trevorsears.com>
2026-03-18 18:56:45 -0400

    Added `PoseBuilder.getHubShootingPose`.

--- src/main/java/frc/robot/util/PoseBuilder.java
+++ src/main/java/frc/robot/util/PoseBuilder.java
@@ -99,6 +99,18 @@ public class PoseBuilder implements Supplier<Pose2d> {
 			.withTranslation(ROBOT_RELATIVE, RobotDimensions.ROBOT_LENGTH.div(2), BACKWARDS);
 
 	}
+	
+	public static PoseBuilder getHubShootingPose(
+		Distance shootingRadius,
+		Angle shootingAngle
+	) {
+		
+		return PoseBuilder.fromPose(new Pose2d(VirtualField.getHubCenterPoint(), Rotation2d.kZero))
+			.withFieldRelativeHeading(RIGHT)
+			.withRotation(shootingAngle)
+			.withTranslation(ROBOT_RELATIVE, shootingRadius, RIGHT);
+		
+	}
 
 //	/**
 //	 * Returns a pose for scoring on the reef, facing the AprilTag with the
